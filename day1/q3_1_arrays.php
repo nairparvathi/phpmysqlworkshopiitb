9937b7f2e4ecc7c6837e83b05be2abf763603ebc
nairparvathi <61344286+nairparvathi@users.noreply.github.com>
2020-10-05 20:53:45 +0530

    Create q3_1_arrays.php

---
+++ day1/q3_1_arrays.php
@@ -0,0 +1,12 @@
+<?php
+
+$days = array("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday");
+
+echo "First day of the week is $days[0]<br>";
+echo "Second day of the week is $days[1]<br>";
+echo "Third day of the week is $days[2]<br>";
+echo "Fourth day of the week is $days[3]<br>";
+echo "Fifth day of the week is $days[4]<br>";
+echo "Sixth day of the week is $days[5]<br>";
+echo "Seventh day of the week is $days[6]<br>";
+?>
