98c87f498052b1451f275632b206c95b0463046b
nairparvathi <61344286+nairparvathi@users.noreply.github.com>
2020-10-05 20:53:37 +0530

    Create q2_1_operators.php

---
+++ day1/q2_1_operators.php
@@ -0,0 +1,25 @@
+<?php
+         $a = 45;
+         $b = 15;
+         
+         $c = $a + $b;
+         echo "Addtion Operation Result: $c <br/>";
+         
+         $c = $a - $b;
+         echo "Substraction Operation Result: $c <br/>";
+         
+         $c = $a * $b;
+         echo "Multiplication Operation Result: $c <br/>";
+         
+         $c = $a / $b;
+         echo "Division Operation Result: $c <br/>";
+         
+         $c = $a % $b;
+         echo "Modulus Operation Result: $c <br/>";
+         
+         $c = $a++; 
+         echo "Increment Operation Result: $c <br/>";
+         
+         $c = $a--; 
+         echo "Decrement Operation Result: $c <br/>";
+      ?>
